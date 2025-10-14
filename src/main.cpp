e6e51435dec3bfcd73bd060720f11f7f699ad2f7
Rarder44 <lpozzi44@gmail.com>
2025-10-14 17:29:02 +0200

    - implementato pre-compiler per convertire le pagine web da HTML a cpp - aggiunto web server per fornire la pagina HTML per l'interazione con l'esp tramite websocket - implementata logica per il funzionamento del mouse ( movimento, click, scroll ) - implementata logica per pressione tasti ( non testata )

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,15 @@
 #include <Adafruit_NeoPixel.h>
 #include <UrlEncode.h>
 #include <ESPmDNS.h> 
+#include <ESPAsyncWebServer.h>
 
 #define LOGGING_ENABLED 1
-#define LOG_ON_WEB 1
+//#define LOG_ON_WEBSOCKET 1
+//#define LOG_ON_SERIAL 1
 #define LED_BUILDIN 48
 
 #include "wifi_credential.h"
+#include "HTML/_include_all.h"
 
 //the file "wifi_credential.h" must contains these 2 variables
 //const char* ssid = "ssid";         
@@ -25,7 +28,7 @@
 
 
 
-const unsigned int localUdpPort = 4210; // Porta UDP di ascolto
+//const unsigned int localUdpPort = 4210; // Porta UDP di ascolto
 const String serverName = "KVESP32"; // Nome del server per mDNS
 
 
@@ -37,42 +40,58 @@ USBHIDMouse Mouse;
 USBHIDKeyboard Keyboard;
 
 
-WiFiUDP udp;
+//WiFiUDP udp;
+
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
 
 
 void manageKeyboardEvent(char incomingPacket[]);
 void manageMouseEvent(char incomingPacket[]);
 void manageCopyEvent(char incomingPacket[]);
+void managePacket(char incomingPacket[] );
+
 
 
 // Log function using Serial.printf
 size_t Log(const char *format, ...)
 {
+ 
   #if LOGGING_ENABLED
-    char loc_buf[128];
-        char *temp = loc_buf;
-        va_list arg;
-        va_list copy;
-        va_start(arg, format);
-        va_copy(copy, arg);
-        int len = vsnprintf(temp, sizeof(loc_buf), format, copy);
-        va_end(copy);
 
-        if (len < 0) {
+  //alloco un buffer e tramite vsnprintf formatto la stringa
+    char loc_buf[128];
+    char *temp = loc_buf;
+    va_list arg;
+    va_list copy;
+    va_start(arg, format);
+    va_copy(copy, arg);
+    int len = vsnprintf(temp, sizeof(loc_buf), format, copy);
+    va_end(copy);
+
+    //Se len < 0 errore di formattazione, si chiude arg e si ritorna 0
+    //Se len >= sizeof(loc_buf), significa che loc_buf non basta ad ospitare la stringa.
+
+    if (len < 0) {
+        va_end(arg);
+        return 0;
+    }
+    if (len >= (int)sizeof(loc_buf)) {
+      //quindi alloco un buffer più grande con la malloc ed uso quello
+        temp = (char *)malloc(len + 1);
+        if (temp == NULL) {
             va_end(arg);
             return 0;
         }
-        if (len >= (int)sizeof(loc_buf)) {
-            temp = (char *)malloc(len + 1);
-            if (temp == NULL) {
-                va_end(arg);
-                return 0;
-            }
-            len = vsnprintf(temp, len + 1, format, arg);
-        }
-        va_end(arg);
+        len = vsnprintf(temp, len + 1, format, arg);
+    }
+    va_end(arg);
 
-    #if LOG_ON_WEB
+
+     
+
+    #if LOG_ON_WEB  //NON USARE!! CRASHAAAA
         // Costruisci la URL
         String msg = urlEncode(String(temp));
 
@@ -82,41 +101,27 @@ size_t Log(const char *format, ...)
         String payload = "msg=" + msg;
         int httpCode = http.POST(payload);
         http.end();
-
-
-    #else
-        /*char loc_buf[64];
-        char * temp = loc_buf;
-        va_list arg;
-        va_list copy;
-        va_start(arg, format);
-        va_copy(copy, arg);
-        int len = vsnprintf(temp, sizeof(loc_buf), format, copy);
-        va_end(copy);
-        if(len < 0) {
-            va_end(arg);
-            return 0;
-        }
-        if(len >= (int)sizeof(loc_buf)){  // comparation of same sign type for the compiler
-            temp = (char*) malloc(len+1);
-            if(temp == NULL) {
-                va_end(arg);
-                return 0;
-            }
-            len = vsnprintf(temp, len+1, format, arg);
-        }
-        va_end(arg);*/
-        len = Serial.write((uint8_t*)temp, len);
-        
-      #endif
-
-      if(temp != loc_buf){
-            free(temp);
-      }
-      return len;
-    #else
-      return 0;
     #endif
+
+    #if LOG_ON_WEBSOCKET
+        ws.textAll(temp);
+    #endif
+
+    #if LOG_ON_SERIAL
+        len = Serial.write((uint8_t*)temp, len);
+        Serial.write('\n');
+    #endif
+
+
+
+
+    if(temp != loc_buf){
+          free(temp);
+    }
+    return len;
+  #else
+    return 0;
+  #endif
 }
 
 void LogColor(int R,int G, int B)
@@ -129,14 +134,15 @@ void LogColor(int R,int G, int B)
 enum keyEvents {
   KEY_UP=0,
   KEY_DOWN=1,
-  KEY_CLICK=2,
+  KEY_PRESS=2,
 };
 
 enum mouseEvents {
   MOUSE_UP=0,
   MOUSE_DOWN=1,
   MOUSE_CLICK=2,
-  MOUSE_MOVE,
+  MOUSE_MOVE=3,
+  MOUSE_WHEEL=4
 };
 
 enum Events{
@@ -162,40 +168,75 @@ enum Events{
   //<value> -> <uint_16_t x> <uint_16_t y> (relative movement)
 
 
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo*)arg;
+  if(info->final && info->index == 0 && info->len == len) {
+    if(len == 6) {
+      char buffer[6];  // 6 byte + terminatore stringa
+      memcpy(buffer, data, 6);
+      managePacket(buffer);
+    } 
+    // Altri casi messaggi possono essere gestiti qui
+  }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,void *arg, uint8_t *data, size_t len) {
+  if(type == WS_EVT_DATA) {
+    handleWebSocketMessage(arg, data, len);
+  }
+}
+
+
 void setup() {  // initialize the buttons' inputs:
 
+  //Led RGB ( status ) 
   strip.begin();
   strip.setBrightness(30);
   strip.show();
 
-  Serial.begin(115200);
-  // initialize mouse control:
-  Mouse.begin();
-  Keyboard.begin();
-  USB.begin();
 
+  //mouse/keyboard control:
+ 
+
+  #if LOG_ON_SERIAL
+    Serial.begin(115200);
+  #else
+    Mouse.begin();
+    Keyboard.begin();
+    USB.begin();
+  #endif
+
+
+  
+  //Wifi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     LogColor(255,0,0);
     delay(250);
     LogColor(0,0,0);
     delay(250);
-    Serial.print(".");
   }
-  Serial.println("\nWiFi connected");
   Log("WiFi connected");
   LogColor(0,255,0);
   delay(250);
   LogColor(0,0,0);
-  udp.begin(localUdpPort);
 
+  //MDNS
   MDNS.begin(serverName);
 
+
+  //web server + websocket
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", PAGE_web);
+  });
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
+  server.begin();
 
 }
 
 //ritorna il numero di byte letti
-int readPacket(char incomingPacket[] )
+/*int readPacket(char incomingPacket[] )
 {
   int packetSize = udp.parsePacket();
   //Log("PacketSize: %d",packetSize);
@@ -207,41 +248,55 @@ int readPacket(char incomingPacket[] )
     }
   }
   return 0; 
+}*/
+
+void managePacket(char incomingPacket[] )
+{
+
+  Log("%u %u %u %u %u %u",
+    (uint8_t)incomingPacket[0],
+    (uint8_t)incomingPacket[1],
+    (uint8_t)incomingPacket[2],
+    (uint8_t)incomingPacket[3],
+    (uint8_t)incomingPacket[4],
+    (uint8_t)incomingPacket[5]
+  );
+  
+
+  //se c'è abilitata la seriale, non può funzionare in HID
+  //#if LOG_ON_SERIAL
+  //  return;
+  //#endif
+
+  
+
+  Events eventType = (Events)incomingPacket[0];
+  switch (eventType)
+  {
+  case Events::KEYBOARD:
+
+    //manageKeyboardEvent(incomingPacket);
+    break;
+
+  case Events::MOUSE :
+    manageMouseEvent(incomingPacket);
+    break;
+
+  case Events::COPY :
+    //manageCopyEvent(incomingPacket);
+    break;
+  case Events::NO_EVENT :
+    Log("No event\n");
+  default:
+  
+    break;
+  }
+  return ; 
 }
 
 
 void loop() {
-
-  char incomingPacket[255];
-  int packetSize = readPacket(incomingPacket);
-  if (packetSize) {
-    Log("Received %d bytes from %s, port %d\n", packetSize, udp.remoteIP().toString().c_str(), udp.remotePort());
-
-    Events eventType = (Events)incomingPacket[0];
-    switch (eventType)
-    {
-    case Events::KEYBOARD:
-
-      manageKeyboardEvent(incomingPacket);
-      break;
-
-    case Events::MOUSE :
-
-      manageMouseEvent(incomingPacket);
-      break;
-
-    case Events::COPY :
-      manageCopyEvent(incomingPacket);
-      break;
-    case Events::NO_EVENT :
-      Log("No event\n");
-    default:
-    
-      break;
-    }
-      
-  }
-
+  ws.cleanupClients();
 }
 
 
@@ -259,7 +314,7 @@ void manageKeyboardEvent(char incomingPacket[])
     Keyboard.press(key);
     break;
 
-  case keyEvents::KEY_CLICK:
+  case keyEvents::KEY_PRESS:
     Keyboard.press(key);
     delay(20);
     Keyboard.release(key);
@@ -272,28 +327,68 @@ void manageKeyboardEvent(char incomingPacket[])
 
 void manageMouseEvent(char incomingPacket[])
 {
+  /*
+                      HID     JS
+      MOUSE_LEFT      0x01    0
+      MOUSE_RIGHT     0x02    2
+      MOUSE_MIDDLE    0x04    1
+      MOUSE_BACKWARD  0x08    3
+      MOUSE_FORWARD   0x10    4
+      MOUSE_ALL       0x1F    
+    */
+
+  const uint8_t jsTiHID[]={0x01,0x04,0x02,0x08,0x10};
+  const int jsTiHIDSize = (sizeof(jsTiHID)/sizeof(jsTiHID[0]));
+  for (int i = 0; i < jsTiHIDSize; i++)
+  {
+    Log("%d",jsTiHID[i]);
+  }
+  
+
+ 
   mouseEvents eventType = (mouseEvents)incomingPacket[1];
-  uint8_t key = (uint8_t)incomingPacket[2];
+  int16_t data = (int16_t)((incomingPacket[2] << 8) | incomingPacket[3]);
+
   switch (eventType)
   {
+    
   case mouseEvents::MOUSE_UP:
-    Mouse.release(key);
-    break;
+   {
+      if (!(data >= 0 && data < jsTiHIDSize)) {
+        Log("ERR: %d - Mouse Key not found!",data);
+        break;
+      }   
+      uint8_t HIDMouseKey = jsTiHID[data];
+      Log("Mouse key released: %u",HIDMouseKey);
+    
+      Mouse.release(HIDMouseKey);
+      break;
+    }
   
   case mouseEvents::MOUSE_DOWN:
-    Mouse.press(key);
-    break;
-
+    {
+      if (!(data >= 0 && data < jsTiHIDSize)) {
+        Log("ERR: %d - Mouse Key not found!",data);
+        break;
+      }   
+      uint8_t HIDMouseKey = jsTiHID[data];
+      Log("Mouse key pressed: %u",HIDMouseKey);
+    
+      Mouse.press(HIDMouseKey);
+      break;
+    }
   case mouseEvents::MOUSE_CLICK:
-    Mouse.press(key);
-    delay(20);
-    Mouse.release(key);
+    //Already managed from up and down
+    //Mouse.press(data);
+    //delay(20);
+    //Mouse.release(data);
     break;
 
    case mouseEvents::MOUSE_MOVE:
     {
-      int16_t x = (int16_t)((incomingPacket[3] << 8) | incomingPacket[4]);
-      int16_t y = (int16_t)((incomingPacket[5] << 8) | incomingPacket[6]);
+      int16_t x = data;
+      int16_t y = (int16_t)((incomingPacket[4] << 8) | incomingPacket[5]);
+      Log("Mouse move: %d %d",x,y);
       Mouse.move(x, y);
     }
     break;
